d62536d837a1be1994b2bac5da61507a422fed50
ruarsv5 <vadim.ershov@raiffeisen.ru>
2025-03-13 02:58:21 +0300

    Implemented module 3 about entity domain model

---
+++ core/src/main/kotlin/ru/ershov/ddd/delivery/core/domain/model/courier/CourierStatus.kt
@@ -0,0 +1,24 @@
+package ru.ershov.ddd.delivery.core.domain.model.courier
+
+class CourierStatus private constructor(val name: String){
+
+    companion object {
+        val FREE = CourierStatus("FREE")
+        val BUSY = CourierStatus("BUSY")
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CourierStatus
+
+        return name == other.name
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
+
+}
