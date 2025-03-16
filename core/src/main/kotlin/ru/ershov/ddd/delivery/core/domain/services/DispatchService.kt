aa212b1d8d064c3b3c0e8d4954ad1a894683ebf0
ruarsv5 <vadim.ershov@raiffeisen.ru>
2025-03-17 01:37:20 +0300

    Implemented module 5 about domain service

---
+++ core/src/main/kotlin/ru/ershov/ddd/delivery/core/domain/services/DispatchService.kt
@@ -0,0 +1,18 @@
+package ru.ershov.ddd.delivery.core.domain.services
+
+import org.springframework.stereotype.Service
+import ru.ershov.ddd.delivery.core.domain.model.courier_aggregate.Courier
+import ru.ershov.ddd.delivery.core.domain.model.order_aggregate.Order
+
+@Service
+class DispatchService() : IDispatchService {
+
+    override fun chooseCourier(order: Order, couriers: List<Courier>): Courier {
+        require(couriers.isNotEmpty()) { "List of Couriers must not be empty" }
+        val targetLocation = order.location
+
+        return couriers
+            .minByOrNull { courier -> courier.timeToLocation(targetLocation) }!! // потому что проверили что не пустой список выше
+    }
+
+}
