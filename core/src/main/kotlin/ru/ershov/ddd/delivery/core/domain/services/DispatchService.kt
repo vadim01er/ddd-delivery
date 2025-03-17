cac5693d041d741857d9ad1442c435b5c5ff3703
ruarsv5 <vadim.ershov@raiffeisen.ru>
2025-03-17 20:56:54 +0300

    Implemented module 5 about domain service

--- core/src/main/kotlin/ru/ershov/ddd/delivery/core/domain/services/DispatchService.kt
+++ core/src/main/kotlin/ru/ershov/ddd/delivery/core/domain/services/DispatchService.kt
@@ -5,14 +5,19 @@ import ru.ershov.ddd.delivery.core.domain.model.courier_aggregate.Courier
 import ru.ershov.ddd.delivery.core.domain.model.order_aggregate.Order
 
 @Service
-class DispatchService() : IDispatchService {
+class DispatchService : IDispatchService {
 
     override fun chooseCourier(order: Order, couriers: List<Courier>): Courier {
         require(couriers.isNotEmpty()) { "List of Couriers must not be empty" }
         val targetLocation = order.location
 
-        return couriers
-            .minByOrNull { courier -> courier.timeToLocation(targetLocation) }!! // потому что проверили что не пустой список выше
+        val chosenCourier = couriers
+            .minByOrNull { courier -> courier.timeToLocation(targetLocation) }!!  // потому что проверили что не пустой список выше
+
+        order.assignCourier(chosenCourier)
+        chosenCourier.busy()
+
+        return chosenCourier
     }
 
 }
